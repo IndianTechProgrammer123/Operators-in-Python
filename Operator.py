fa0cb215f0f0c8d6accf62d0fb8ed308b6991a22
Indian Tech Programmer <112259649+IndianTechProgrammer123@users.noreply.github.com>
2022-09-04 12:52:53 +0530

    Add files via upload

---
+++ Operator.py
@@ -0,0 +1,20 @@
+# Operators in Python
+# _____________________________________________________
+#|_____Operation_____|____Operators____|____Outputs____|
+#|     Addition      |        +        |    2+3 = 5    |
+#|   Subtraction     |        -        |    2-3 = 1    |
+#|  Multiplication   |        *        |    2*3 = 6    | 
+#|     Exponent      |       **        |    2**3 = 8   |
+#|  True Division    |        /        |    4/2 = 2.0  |
+#|  Floor division   |       //        |    4//2 = 2   |
+#| Remainder(Modulo) |        %        |    6%2 = 0    |
+#|_____________________________________________________|
+
+# Output
+print(2+3)
+print(3-2)
+print(2*3)
+print(2**3)
+print(4/2)
+print(4//2)
+print(6%2)
